175f288491620add8d9606b67aa6676cd22b6908
Mary Arzumanyan <mary.arzumanyan@gmail.com>
2018-10-17 23:32:31 -0700

    Main functionality completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,20 +14,20 @@ function Challenge( singer, imgUrl, songName, songUrl ) {
   var hangman =
   {
       challenges : [
-          new Challenge("Aerosmith", "", ""),
-          new Challenge("Beatles", "", ""),
-          new Challenge("Madonna", "", ""),
-          new Challenge("Queen", "", ""),
-          new Challenge("GunsNRoses", "", ""),
-          new Challenge("Scorpions", "", ""),
-          new Challenge("Jackson", "", ""),
-          new Challenge("RollingStones", "", ""),
-          new Challenge("Blondie", "", ""),
-          new Challenge("Yanni", "", "")
+          new Challenge("Aerosmith", "assets/images/aerosmith.jpg", "Pink", "https://www.youtube.com/watch?v=KXvvK0CcJZQ"),
+          new Challenge("Beatles", "assets/images/beatles.jpg", "Strawberry Fields Forever", "https://www.youtube.com/watch?v=B32dBS_1vMc"),
+          new Challenge("Madonna", "assets/images/madonna.jpg", "La Isla Bonita", "https://www.youtube.com/watch?v=JZZWHnK6piM"),
+          new Challenge("Queen", "assets/images/queen.jpg", "Bicycle Race", "https://www.youtube.com/watch?v=f_uvlq8xsy4"),
+          new Challenge("GunsNRoses", "assets/images/gunsNRoses.jpg", "Sweet Child O'Mine", "https://www.youtube.com/watch?v=nnJUiWahYhg"),
+          new Challenge("Scorpions", "assets/images/scorpions.jpg", "House Of Cards", "https://www.youtube.com/watch?v=JJSsuP_5Ld4"),
+          new Challenge("Jackson", "assets/images/jackson.jpg", "Billie Jean", "https://www.youtube.com/watch?v=oUlXIRYtetg"),
+          new Challenge("RollingStones", "assets/images/rollingStones.jpg", "Paint It, Black", "https://www.youtube.com/watch?v=-YJIvuTWGQw"),
+          new Challenge("Blondie", "assets/images/blondie.jpg", "Call me", "https://www.youtube.com/watch?v=y6QBaZHltJw"),
+          new Challenge("ModernTalking", "assets/images/modernTalking.jpg", "You're My Heart", "https://www.youtube.com/watch?v=Agiasy89pLo")
       ],
       attemptsLeft : 10,
       wins : 0,
-      word : "",
+      word : [],
       letters : [],
       currentChallenge : -1,
 
@@ -36,7 +36,7 @@ function Challenge( singer, imgUrl, songName, songUrl ) {
       },
 
       isWordGuessed : function() {
-        return !this.word.includes('-');
+        return !this.word.includes("_");
       },
 
       isGameOver : function() {
@@ -44,11 +44,13 @@ function Challenge( singer, imgUrl, songName, songUrl ) {
       },
 
       newGame : function() {
+
+          // removes previous chalange record from database
           if (this.currentChallenge != -1) {
               this.challenges.splice(this.currentChallenge, 1);
           }
 
-          if (this.challenges.length == 0) {
+          if (this.challenges.length === 0) {
               return;
           }
 
@@ -57,21 +59,40 @@ function Challenge( singer, imgUrl, songName, songUrl ) {
           }
           while (this.currentChallenge === this.challenges.length);
 
-          this.word = "-".repeat(this.challenges[this.currentChallenge].singer.length);
-          this.attemptsLeft = 10;
+          this.word = [];
+          for(i = 0; i < this.challenges[this.currentChallenge].singer.length; ++ i)
+          {
+            this.word[i] = "_";
+          }
+          
+          this.attemptsLeft = 15;
           this.letters = [];
+          this.songUrl = this.challenges[this.currentChallenge].songUrl;
+          song.src = hangman.currentSongSrc();
       },
 
       currentWordReveal : function() {
-          return this.word;
+          return this.word.join(" ");
       },
+
+      currentImageSrc : function() {
+          return this.challenges[this.currentChallenge].imgUrl;
+      },
+
+      currentSongSrc : function() {
+          return this.challenges[this.currentChallenge].songUrl;
+      },
+
+      currentSongName : function() {
+          return this.challenges[this.currentChallenge].songName + " By " + this.challenges[this.currentChallenge].singer;
+      },
+
 
       makeGuess : function(letter) {
           if (this.letters.includes(letter)) {
               // Ignore
               return;
           }
-
           -- this.attemptsLeft;
           this.letters[this.letters.length] = letter;
           var singer = this.challenges[this.currentChallenge].singer;
@@ -81,4 +102,6 @@ function Challenge( singer, imgUrl, songName, songUrl ) {
               }
           }
       }
+
+
   }
